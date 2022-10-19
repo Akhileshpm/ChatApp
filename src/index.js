42bfd107fd17e0f63b57cc99bc22bf02c9934d40
akhilesh <akhileshpm333@gmail.com>
2022-10-19 23:17:34 +0530

    added socket.io package, replaced server with http, added socketio client side first steps

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+io.on('connection', ()=>{
+    console.log("new connection");
+})
+
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`server is up on ${port} `);
 })
